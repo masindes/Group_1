80d1d2bbd4bb46ca37b155106b1504cf35de53ac
Louis <yienlouis470@gmail.com>
2025-02-18 11:32:53 +0300

    Initial commit

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const numbers = ['1', '2', '3', '4', '5'];
-console.log(numbers);
+console.log("Before pop:", numbers);
+
+const removedElement = numbers.pop(); 
+console.log("After pop:", numbers);
+console.log("Removed element:", removedElement);
